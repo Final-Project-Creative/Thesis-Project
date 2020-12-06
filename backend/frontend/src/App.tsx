8955d099b2f9a5be85f3a6907cc4c0e7cdd5086b
aya abuyousef <aya.abuyousef@gmail.com>
2020-12-06 23:34:11 +0200

    TSX

--- backend/frontend/src/App.tsx
+++ backend/frontend/src/App.tsx
@@ -1,25 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from './Component/Footer/Footer.js';
+// import SignUp from './Component/SignUp/SignUp.js'
+// import PhoneInput from 'react-phone-input-2'
+// import 'react-phone-input-2/lib/style.css'
+
+// {/* <PhoneInput
+//   country={'us'}
+//   value={this.state.phone}
+//   onChange={phone => this.setState({ phone })}
+// /> */}
+
+
 
 function App() {
   return (
     <div className="App">
+      {/* <SignUp /> */}
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>RAWNAQ.COM</h1>
+        <img id ='img' src="https://image.freepik.com/free-vector/shopping-online-website-mobile-application-landing-page-concept-marketing-digital-marketing_144352-77.jpg"  alt="" />
       </header>
+      <div>
+      <Footer></Footer>
+      
+      </div>
+      
     </div>
+   
+   
+    
   );
 }
 
