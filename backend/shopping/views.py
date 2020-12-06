1cd0d2c927495bbb19003c32b3456b5d9512ae23
Rasha <r.alamleh@gmail.com>
2020-12-06 19:28:10 +0200

    app urls and views

--- backend/shopping/views.py
+++ backend/shopping/views.py
@@ -51,3 +51,27 @@ def add_to_cart(request, pk) :
         return redirect("core:product", pk = pk)
 
 
+def remove_from_cart(request, pk):
+    item = get_object_or_404(Item, pk=pk )
+    order_qs = Order.objects.filter(
+        user=request.user, 
+        ordered=False
+    )
+    if order_qs.exists():
+        order = order_qs[0]
+        if order.items.filter(item__pk=item.pk).exists():
+            order_item = OrderItem.objects.filter(
+                item=item,
+                user=request.user,
+                ordered=False
+            )[0]
+            order_item.delete()
+            messages.info(request, "Item \""+order_item.item.item_name+"\" remove from your cart")
+            return redirect("core:product")
+        else:
+            messages.info(request, "This Item not in your cart")
+            return redirect("core:product", pk=pk)
+    else:
+        #add message doesnt have order
+        messages.info(request, "You do not have an Order")
+        return redirect("core:product", pk = pk)
